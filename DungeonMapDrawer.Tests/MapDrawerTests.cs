4ab3da5627d105b5da8191748c4d727c4f4b4e28
ajackson <ajackson@ebsco.com>
2016-08-15 15:43:14 -0400

    Adding initial drawer project code. includes a test for image dimensions

---
+++ DungeonMapDrawer.Tests/MapDrawerTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace DungeonMapDrawer.Tests
+{
+    [TestFixture]
+    public class MapDrawerTests
+    {
+        private MapDrawer _systemUnderTest;
+
+        [SetUp]
+        public void BeforeEach()
+        {
+            _systemUnderTest = new MapDrawer();
+        }
+
+        [Test]
+        public void GivenDimensionsAndTilePixelSize_MapSizeCorrect()
+        {
+            _systemUnderTest.SetTileSize(10);
+            _systemUnderTest.SetMapTileDimensions(10, 10);
+
+            Assert.AreEqual(100, _systemUnderTest.PixelHeight);
+            Assert.AreEqual(100, _systemUnderTest.PixelWidth);
+        }
+    }
+}
---
+++ DungeonMapDrawer/MapDrawer.cs
@@ -0,0 +1,26 @@
+﻿namespace DungeonMapDrawer
+{
+    public class MapDrawer
+    {
+        private int _tilePixelHeight;
+        private int _tilePixelWidth;
+        private int _mapTileHeight;
+        private int _mapTileWidth;
+
+        public int PixelHeight { get {return _tilePixelHeight * _mapTileHeight;} }
+        public int PixelWidth { get {return _tilePixelWidth * _mapTileWidth;} }
+        public void SetTileSize(int height, int width = 0)
+        {
+            _tilePixelHeight = height;
+            _tilePixelWidth = width > 0 ? width : height;
+        }
+
+        public void SetMapTileDimensions(int height, int width = 0)
+        {
+            _mapTileHeight = height;
+            _mapTileWidth = width > 0 ? width : height;
+        }
+
+        
+    }
+}
