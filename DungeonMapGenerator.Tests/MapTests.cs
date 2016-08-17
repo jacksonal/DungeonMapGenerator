2e4c8227e4fcd3080b779d809a515fa0ea22c3a1
Alex Jackson <alex.lee.jackson@gmail.com>
2016-08-16 21:46:40 -0400

    map model sizing tests

--- DungeonMapGenerator.Tests/MapTests.cs
+++ DungeonMapGenerator.Tests/MapTests.cs
@@ -1,10 +1,26 @@
-﻿using NUnit.Framework;
+﻿using DungeonMapGenerator.Model;
+using NUnit.Framework;
 
 namespace DungeonMapGenerator.Tests
 {
     [TestFixture]
     public class MapTests
     {
+        private Map _systemUnderTest;
 
+        [SetUp]
+        public void BeforeEach()
+        {
+            _systemUnderTest = new Map();
+        }
+
+        [Test]
+        public void MapHeightAndWidth_TileDimensions_ReturnInteger()
+        {
+            _systemUnderTest.Size = MapSize.Medium;
+
+            Assert.Greater(_systemUnderTest.TileHeight,0);
+            Assert.Greater(_systemUnderTest.TileWidth, 0);
+        }
     }
 }
