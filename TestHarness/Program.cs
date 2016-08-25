148c51dd0c6e106dc4da910a216b12126142a503
Alex Jackson <alex.lee.jackson@gmail.com>
2016-08-24 20:21:45 -0400

    adding test harness project for running the dungeon generator

---
+++ TestHarness/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DungeonMapDrawer;
+
+namespace TestHarness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var drawer = new MapDrawer();
+            drawer.SetTileSize(50);
+            drawer.SetMapTileDimensions(20);
+            var mapImage = drawer.DrawMap();
+            mapImage.Save("map.png");
+        }
+    }
+}
