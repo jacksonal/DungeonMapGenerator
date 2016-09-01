ca134ff946b3276dfa66587a5711342e49acc57a
Alex Jackson <alex.lee.jackson@gmail.com>
2016-08-31 20:28:43 -0400

    adding a map validator. checks validity of a room in a map

---
+++ DungeonMapGenerator.Tests/ValidatorTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Configuration;
+using DungeonMapGenerator.Model;
+using NUnit.Framework;
+
+namespace DungeonMapGenerator.Tests
+{
+    [TestFixture]
+    public class ValidatorTests
+    {
+        private MapValidator _systemUnderTest;
+
+        [SetUp]
+        public void BeforeEach()
+        {
+            _systemUnderTest = new MapValidator();
+        }
+
+        [Test]
+        public void IsRoomValid_EmptyRoomList_RoomIsValid()
+        {
+            Assert.IsTrue(_systemUnderTest.IsRoomValid(new Map(), new Room()));
+        }
+
+        [Test]
+        public void IsRoomValid_NoCollidingRooms_RoomIsValid()
+        {
+            var map = new Map();
+            map.AddRoom(new Room {Height = 4,Width = 4,XCoord = 0,YCoord = 0});
+            Assert.IsTrue(_systemUnderTest.IsRoomValid(map,new Room {Height = 4, Width = 4, XCoord = 5,YCoord = 1}));
+        }
+
+        [Test]
+        public void IsRoomValid_RoomsCollide_RoomIsNotValid()
+        {
+            var map = new Map();
+            map.AddRoom(new Room { Height = 4, Width = 4, XCoord = 0, YCoord = 0 });
+            Assert.IsFalse(_systemUnderTest.IsRoomValid(map, new Room { Height = 4, Width = 4, XCoord = 1, YCoord = 1 }));
+        }
+
+        [Test]
+        public void IsRoomValid_RoomOutsideMapBounds_RoomIsNotValid()
+        {
+            var map = new Map {Size = MapSize.Small};
+            Assert.IsFalse(_systemUnderTest.IsRoomValid(map, new Room { Height = 4, Width = 4, XCoord = -1, YCoord = -1 }));
+        }
+        [Test]
+        public void IsRoomValid_RoomInsideMapBounds_RoomIsValid()
+        {
+            var map = new Map { Size = MapSize.Small };
+            Assert.IsTrue(_systemUnderTest.IsRoomValid(map, new Room { Height = 4, Width = 4, XCoord = 1, YCoord = 1 }));
+        }
+    }
+}
---
+++ DungeonMapGenerator/MapValidator.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using DungeonMapGenerator.Model;
+
+namespace DungeonMapGenerator
+{
+    public class MapValidator
+    {
+        public bool IsRoomValid(IMap map, IRoom toAdd)
+        {
+            var ret = MapContainsRoom(map, toAdd); ;
+            if (map.Rooms.Any() && ret)
+            {
+                ret = !map.Rooms.Any(r => DoRoomsCollide(r, toAdd));
+            }
+            return ret;
+        }
+
+        private bool MapContainsRoom(IMap map, IRoom room)
+        {
+            return (room.XCoord >= 0) &&
+                   (room.YCoord >= 0) &&
+                   (room.XCoord + room.Width <= map.TileWidth) &&
+                   (room.YCoord + room.Height <= map.TileHeight);
+        }
+
+        private bool DoRoomsCollide(IRoom room1, IRoom room2)
+        {
+            return room1.XCoord < room2.XCoord + room2.Width &&
+                   room1.XCoord + room1.Width > room2.XCoord &&
+                   room1.YCoord < room2.YCoord + room2.Height &&
+                   room1.Height + room1.YCoord > room2.YCoord;
+        }
+    }
+}
