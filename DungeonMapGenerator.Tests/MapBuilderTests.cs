6836ced94333969dde30e47251cb22e77d0fb0a1
Alex Jackson <alex.lee.jackson@gmail.com>
2016-09-02 22:28:25 -0400

    random coordinate generator

---
+++ DungeonMapGenerator.Tests/MapBuilderTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace DungeonMapGenerator.Tests
+{
+    [TestFixture]
+    public class MapBuilderTests
+    {
+        private MapBuilder _systemUnderTest;
+
+        [SetUp]
+        public void BeforeEach()
+        {
+            _systemUnderTest = new MapBuilder();
+        }
+
+        [Test]
+        public void GetRandomCoords_GivenMaxValues_ReturnedCoordsWithinRange()
+        {
+	        var coord = _systemUnderTest.GetRandomLocation(10, 10);
+			Assert.LessOrEqual(coord.XCoord,10);
+			Assert.LessOrEqual(coord.YCoord, 10);
+
+			Assert.GreaterOrEqual(coord.XCoord,0);
+			Assert.GreaterOrEqual(coord.YCoord, 0);
+		}
+    }
+}
---
+++ DungeonMapGenerator/MapBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DungeonMapGenerator
+{
+    public class MapBuilder
+    {
+	    private int DEFAULT_SEED = 12345;
+	    private Random _random;
+
+	    public MapBuilder()
+	    {
+		    _random = new Random(DEFAULT_SEED);
+
+	    }
+
+	    public MapBuilder(int seed)
+	    {
+			_random = new Random(seed);
+		}
+
+	    public MapCoordinate GetRandomLocation(int width, int height)
+	    {
+		    return new MapCoordinate(_random.Next(0,width),_random.Next(0,height));
+	    }
+    }
+
+	public class MapCoordinate
+	{
+		public int XCoord { get; }
+		public int YCoord { get; }
+
+		public MapCoordinate(int xCoord, int yCoord)
+		{
+			XCoord = xCoord;
+			YCoord = yCoord;
+		}
+	}
+}
