5548b6d96ba9945a6bf489bc69a6cb28f0ffdd29
Alex Jackson <alex.lee.jackson@gmail.com>
2016-09-05 13:24:57 -0400

    random room size generation. establishing rules for minimum room dimensions

--- DungeonMapGenerator.Tests/MapBuilderTests.cs
+++ DungeonMapGenerator.Tests/MapBuilderTests.cs
@@ -23,5 +23,42 @@ namespace DungeonMapGenerator.Tests
 			Assert.GreaterOrEqual(coord.XCoord,0);
 			Assert.GreaterOrEqual(coord.YCoord, 0);
 		}
-    }
+
+	    [Test]
+	    public void GetRandomRoomSize_GivenMaxValues_ReturnsDimensionsWithinRange()
+	    {
+			var coord = _systemUnderTest.GetRandomRoomSize(10, 10);
+			Assert.LessOrEqual(coord.Width, 10);
+			Assert.LessOrEqual(coord.Height, 10);
+
+			Assert.GreaterOrEqual(coord.Width, 0);
+			Assert.GreaterOrEqual(coord.Height, 0);
+		}
+
+		/// <summary>
+		/// a room should meet certain criteria.
+		/// ---
+		/// |_|		This is a one square room its dimensions are 3x3. we shouldn't generate rooms this small
+		/// ---
+		/// ----
+		/// |__|	This is not a room. it is a hallway. the dimensions are 4x3. we shouldn't generate rooms that have a functional width of 1.
+		/// ----
+		/// ----
+		/// |__|	This IS a room.
+		/// |__|
+		/// ----
+		/// 
+		/// A room has a height and width of at least 4.
+		/// </summary>
+		[Test]
+		public void GetRandomRoomSize_ReturnsDimensionsWithinOfAppropriateSize()
+		{
+			var coord = _systemUnderTest.GetRandomRoomSize(10, 10);
+			Assert.LessOrEqual(coord.Width, 10);
+			Assert.LessOrEqual(coord.Height, 10);
+
+			Assert.GreaterOrEqual(coord.Width, 4);
+			Assert.GreaterOrEqual(coord.Height, 4);
+		}
+	}
 }
--- DungeonMapGenerator/MapBuilder.cs
+++ DungeonMapGenerator/MapBuilder.cs
@@ -5,6 +5,7 @@ namespace DungeonMapGenerator
     public class MapBuilder
     {
 	    private int DEFAULT_SEED = 12345;
+	    private int MINIMUM_ROOM_SIZE = 4;
 	    private Random _random;
 
 	    public MapBuilder()
@@ -18,11 +19,28 @@ namespace DungeonMapGenerator
 			_random = new Random(seed);
 		}
 
-	    public MapCoordinate GetRandomLocation(int width, int height)
+	    public MapCoordinate GetRandomLocation(int maxWidth, int maxHeight)
 	    {
-		    return new MapCoordinate(_random.Next(0,width),_random.Next(0,height));
+		    return new MapCoordinate(_random.Next(0, maxWidth),_random.Next(0, maxHeight));
 	    }
+
+	    public RoomSize GetRandomRoomSize(int maxWidth, int maxHeight)
+	    {
+			return new RoomSize(_random.Next(MINIMUM_ROOM_SIZE, maxWidth), _random.Next(MINIMUM_ROOM_SIZE, maxHeight));
+		}
     }
+
+	public class RoomSize
+	{
+		public int Width { get; }
+		public int Height { get; }
+
+		public RoomSize(int width, int height)
+		{
+			Width = width;
+			Height = height;
+		}
+	}
 
 	public class MapCoordinate
 	{
